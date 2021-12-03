492c4fbffebecb6b940ace522c9203653c271089
bonah <bonahbruce93@gmail.com>
2021-12-04 01:53:04 +0900

    2.0 Initialization

---
+++ src/components/Router.js
@@ -0,0 +1,15 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route>
+
+                </Route>
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRouter;
