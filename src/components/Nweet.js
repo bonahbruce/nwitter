b3a1e65430f28aaff5205f1b334da95ed0694820
bonah koo <guavaguava0618@gmail.com>
2021-12-09 16:29:25 +0900

    7.0

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,4 +1,4 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import { useState} from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
@@ -8,11 +8,12 @@ const Nweet = ({ nweetObj, isOwner }) => {
 
     const onDeleteClick = async () => {
         const ok = window.confirm("삭제하시겠습니까?");
-        console.log(ok);
+        
         if (ok) {
-            console.log(nweetObj.id);
-            const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
-            console.log(data); 
+            
+            await dbService.doc(`nweets/${nweetObj.id}`).delete();
+            if (nweetObj.attachmentUrl !== "")
+                await storageService.refFromURL(nweetObj.attachmentUrl).delete(); 
         }
     };
 
@@ -47,6 +48,12 @@ const Nweet = ({ nweetObj, isOwner }) => {
 
 
             <h4>{nweetObj.text}</h4>
+            {nweetObj.attachmentUrl && (
+            <img src={nweetObj.attachmentUrl} width="100px" height="100px" />
+            )}
+
+
+
             {isOwner && (
                 <>
                 <button onClick={onDeleteClick}>Delete Nweet</button>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,12 @@
-import { dbService} from "fbase";
+import { dbService, storageService} from "fbase";
 import { useEffect, useState } from "react";
-import Nweet from "components/Nweet"
-
+import Nweet from "components/Nweet";
+import { v4 as uuidv4 } from "uuid";
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
     const [attachment, setAttachment] = useState("");
-    
+        
     useEffect(() => {
         dbService.collection("nweets").onSnapshot((snapshot) => {
             const newArray = snapshot.docs.map((document) => ({
@@ -18,13 +18,26 @@ const Home = ({ userObj }) => {
     }, []);
 
     const onSubmit = async(event) => {
-        event.preventDefault();
+        event.preventDefault();        
+        
+        let attachmentUrl = "";
+        if (attachment !== "") {
+            const attachmentRef = storageService
+            .ref()
+            .child(`${userObj.uid}/${uuidv4()}`);
+            const response = await attachmentRef.putString(attachment, "data_url");
+            attachmentUrl = await response.ref.getDownloadURL();
+        }
+        
         await dbService.collection("nweets").add({
             text: nweet,
             createdAt: Date.now(),
             creatorId: userObj.uid,
+            attachmentUrl,
         });
         setNweet("");
+        setAttachment("");
+
     };
 
     const onChange = (event) => {
@@ -65,7 +78,7 @@ const Home = ({ userObj }) => {
             <input type="submit" value="Nweet"/>
             {attachment && (
                 <div>
-                    <img src={attachment} width="50px" height="50px"/>
+                    <img src={attachment} width="50px" height="50px" />
                     <button onClick={onClearAttachment}>Clear</button>
                 </div>
             )}   
@@ -83,4 +96,4 @@ const Home = ({ userObj }) => {
     );
 };
 
-export default Home;
+export default Home; 
